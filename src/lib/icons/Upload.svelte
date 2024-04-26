2f744c624945ff4bd956cd5aea6713cc26eb111e
paparomatoe <papatomatoe.project@gmail.com>
2024-04-26 22:05:50 +0300

    add icons for Editor buttons

---
+++ src/lib/icons/Upload.svelte
@@ -0,0 +1,7 @@
+<svg width="24" height="24" focusable="false"
+	><path
+		d="M19 4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-4v-2h4V8H5v10h4v2H5a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h14Zm-8 9.4-2.3 2.3a1 1 0 1 1-1.4-1.4l4-4a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1-1.4 1.4L13 13.4V20a1 1 0 0 1-2 0v-6.6Z"
+		fill-rule="nonzero"
+		fill="var(--color--icon)"
+	/></svg
+>
