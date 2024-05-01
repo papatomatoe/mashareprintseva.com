54c8810dc39c4892d56abf3e7efa597fd71c74b9
papatomatoe <papatomatoe.project@gmail.com>
2024-05-01 12:54:36 +0300

    add sanitize html

--- src/routes/(admin)/admin/(layout)/main-page/+page.server.ts
+++ src/routes/(admin)/admin/(layout)/main-page/+page.server.ts
@@ -1,6 +1,7 @@
 import { redirect, fail } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 import { db } from '$lib/database/db';
+import xss from 'xss';
 
 export const load = (async ({ locals }) => {
 	if (!locals.user) throw redirect(302, '/admin/login');
@@ -24,11 +25,13 @@ export const actions = {
 			return fail(400, { invalid: true });
 		}
 
+		const sanitizedContent = xss(content);
+
 		try {
 			await db.mainPage.upsert({
 				where: { title: 'mainPage' },
 				update: { published, content },
-				create: { title: 'mainPage', slug: '', published, content }
+				create: { title: 'mainPage', slug: '', content: sanitizedContent, published }
 			});
 			return { success: true };
 		} catch (e) {
