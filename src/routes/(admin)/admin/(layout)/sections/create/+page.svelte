786bd7976d50dfb046cc5b95a4583a0140fa5b96
paparomatoe <papatomatoe.project@gmail.com>
2024-05-01 16:23:37 +0300

    fix markup, add projects selection

--- src/routes/(admin)/admin/(layout)/sections/create/+page.svelte
+++ src/routes/(admin)/admin/(layout)/sections/create/+page.svelte
@@ -5,7 +5,18 @@
 	import slugify from 'slugify';
 	import Textarea from '$lib/components/Textarea.svelte';
 	import Editor from '$lib/components/Editor.svelte';
+	import Select from '$lib/components/Select.svelte';
+	import type { ComponentEvents } from 'svelte';
 
+	let projects = [
+		{ id: '1', title: 'The Unseen Hues', published: true },
+		{ id: '2', title: 'Serendipity of Man', published: true },
+		{ id: '3', title: 'Intricate Proportion No.21', published: true },
+		{ id: '4', title: 'In a Former Home', published: false },
+		{ id: '5', title: 'Vortex Constellation', published: true }
+	];
+
+	let selectedProjects = projects;
 	let order = '';
 	let orderError = '';
 	let title = '';
@@ -16,10 +27,14 @@
 	let imageUrlError = '';
 	let published = false;
 
+	$: projectsValue = JSON.stringify(selectedProjects);
+
 	$: slug = slugify(title, { lower: true });
 
 	$: hasErrors =
 		Boolean(imageUrlError) || Boolean(titleError) || Boolean(slugError) || Boolean(orderError);
+
+	$: options = projects.map((el) => ({ title: el.title, value: el.id }));
 
 	const handleRegenerateSlug = () => {
 		slug = slugify(title, { lower: true });
@@ -46,6 +61,18 @@
 		if (!order) orderError = 'required';
 		if (!title) titleError = 'required';
 		if (!slug) slugError = 'required';
+	};
+
+	const handleDeleteProject = (id: string) => {
+		selectedProjects = selectedProjects.filter((project) => project.id !== id);
+	};
+
+	const handleSelectProject = (e: ComponentEvents<Select>['select']) => {
+		const selectedProject = projects.find((el) => el.id === e.detail.value);
+
+		if (!selectedProject || selectedProjects.find((el) => el.id === e.detail.value)) return;
+
+		selectedProjects = [...selectedProjects, selectedProject];
 	};
 </script>
 
@@ -98,12 +125,37 @@
 	<div class="editor row row--bottom">
 		<Editor name="content" label="content" />
 	</div>
-	<div slot="side" />
+	<div class="section-form__side" slot="side">
+		<Select
+			select2
+			label="projects"
+			placeholder="select project..."
+			{options}
+			on:select={handleSelectProject}
+		/>
+		{#if selectedProjects.length}
+			<ul class="projects">
+				{#each selectedProjects as project (project.id)}
+					<li class="project">
+						<div class="project__status" class:project__status--published={project.published} />
+						<h3 class="project__title">{project.title}</h3>
+						<button
+							class="button project__button"
+							type="button"
+							on:click={() => handleDeleteProject(project.id)}
+							aria-label="remove project"
+						/>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+		<input name="projects" type="hidden" readonly bind:value={projectsValue} />
+	</div>
 </Form>
 
 <style>
 	.row {
-		grid-column: 1 /2;
+		grid-column: 1 / -1;
 		display: grid;
 		gap: 30px;
 	}
@@ -114,7 +166,7 @@
 	.row--mid {
 		grid-row: 2 /3;
 		grid-auto-flow: column;
-		grid-template-columns: 320px 1fr;
+		grid-template-columns: 400px 1fr;
 		grid-template-rows: min-content min-content;
 	}
 	.row--bottom {
@@ -123,6 +175,53 @@
 
 	.subtitle {
 		grid-row: 1/ -1;
+	}
+
+	.projects {
+		padding: 15px 8px;
+		display: grid;
+		gap: 15px;
+	}
+	.project {
+		display: grid;
+		grid-template-columns: 7px 1fr 30px;
+		align-items: center;
+		gap: 10px;
+	}
+
+	.project__status {
+		width: 7px;
+		height: 7px;
+		border-radius: 50%;
+		background-color: var(--color--red-light);
+	}
+
+	.project__status--published {
+		background-color: var(--color--green-light);
+	}
+
+	.project__button {
+		position: relative;
+		width: 30px;
+		height: 30px;
+		background-color: transparent;
+	}
+	.project__button::after {
+		position: absolute;
+		content: '';
+		width: 16px;
+		height: 3px;
+		top: 50%;
+		left: 50%;
+		translate: -50%;
+		background-color: var(--color--gray-85);
+	}
+	.project__button:hover,
+	.project__button:focus-visible {
+		background-color: var(--color--primary);
+	}
+	.project__button:active {
+		background-color: var(--color--gray-30);
 	}
 	:global(.subtitle .field) {
 		height: 100%;
