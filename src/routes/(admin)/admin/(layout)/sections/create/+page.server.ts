6d3f7c8e6d4066d743e33daa61325f929b95c16f
paparomatoe <papatomatoe.project@gmail.com>
2024-05-02 21:53:53 +0300

    add section form component

--- src/routes/(admin)/admin/(layout)/sections/create/+page.server.ts
+++ src/routes/(admin)/admin/(layout)/sections/create/+page.server.ts
@@ -2,6 +2,7 @@ import { createSection, db } from '$lib/database/db';
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 import xss from 'xss';
+import { OPTIONS } from '$lib/constants/xssConfig';
 
 export const load = (async ({ locals }) => {
 	if (!locals.user) redirect(302, '/admin/login');
@@ -22,6 +23,7 @@ export const actions = {
 		const published = Boolean(data.get('published'));
 		const content = data.get('content');
 		const image = data.get('image');
+		const thumbnail = data.get('preview');
 		const title = data.get('title');
 		const subtitle = data.get('subtitle');
 		const slug = data.get('slug');
@@ -32,6 +34,7 @@ export const actions = {
 			typeof order !== 'number' ||
 			typeof content !== 'string' ||
 			typeof image !== 'string' ||
+			typeof thumbnail !== 'string' ||
 			typeof title !== 'string' ||
 			typeof subtitle !== 'string' ||
 			typeof slug !== 'string' ||
@@ -40,7 +43,8 @@ export const actions = {
 			return fail(400, { invalid: true });
 		}
 
-		const sanitizedContent = xss(content);
+		// const sanitizedContent = xss(content, OPTIONS); // NOTE: some unexpected behavior, leave unsanitized.
+		const sanitizedContent = content;
 
 		const response = await createSection({
 			published,
@@ -49,6 +53,7 @@ export const actions = {
 			slug,
 			subtitle,
 			image,
+			thumbnail,
 			altTitle,
 			content: sanitizedContent,
 			...(projects.length && { projects })
