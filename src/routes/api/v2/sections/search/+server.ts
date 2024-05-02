5becf026e71d98550d7796ae771c56af98b92b6e
paparomatoe <papatomatoe.project@gmail.com>
2024-05-02 21:50:11 +0300

    add sections api routs

---
+++ src/routes/api/v2/sections/search/+server.ts
@@ -0,0 +1,17 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { db } from '$lib/database/db';
+
+export const POST: RequestHandler = async ({ request }) => {
+	const search = await request.json();
+
+	try {
+		const response = await db.section.findMany({
+			where: { OR: [{ title: { contains: search } }, { slug: { contains: search } }] }
+		});
+
+		return json(response);
+	} catch (e) {
+		throw error(500, 'Something went wrong...');
+	}
+};
