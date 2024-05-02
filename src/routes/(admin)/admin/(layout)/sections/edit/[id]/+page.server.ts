bdafedfe6722472810cb3ac7c7d5566887cdc714
paparomatoe <papatomatoe.project@gmail.com>
2024-05-02 21:51:27 +0300

    add edit section page

---
+++ src/routes/(admin)/admin/(layout)/sections/edit/[id]/+page.server.ts
@@ -0,0 +1,81 @@
+import { createSection, getSection, updateSection } from '$lib/database/db';
+import { error, fail, redirect, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import xss from 'xss';
+import { OPTIONS } from '$lib/constants/xssConfig';
+
+export const load: PageServerLoad = async ({ locals, params }) => {
+	if (!locals.user) redirect(302, '/admin/login');
+
+	const { id } = params;
+
+	const section = await getSection(id);
+
+	if (!section) return error(404, 'not found');
+
+	const breadcrumbs = [
+		{ title: 'dashboard', path: '/admin/dashboard' },
+		{ title: 'sections', path: '/admin/sections' },
+		{ title: section.title, path: `/admin/sections/edit/${section.id}` }
+	];
+
+	return {
+		pageTitle: `Admin | ${section.title ?? 'Section'} Edit`,
+		section,
+		breadcrumbs
+	};
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+
+		const id = String(data.get('id'));
+
+		const order = Number(data.get('order'));
+		const published = Boolean(data.get('published'));
+		const content = data.get('content');
+		const image = data.get('image');
+		const thumbnail = data.get('preview');
+		const title = data.get('title');
+		const subtitle = data.get('subtitle');
+		const slug = data.get('slug');
+		const altTitle = data.get('altTitle');
+		const projects = JSON.parse(String(data.get('projects')));
+
+		if (
+			typeof order !== 'number' ||
+			typeof content !== 'string' ||
+			typeof image !== 'string' ||
+			typeof thumbnail !== 'string' ||
+			typeof title !== 'string' ||
+			typeof subtitle !== 'string' ||
+			typeof slug !== 'string' ||
+			typeof altTitle !== 'string'
+		) {
+			return fail(400, { invalid: true });
+		}
+
+		// const sanitizedContent = xss(content, OPTIONS); // NOTE: some unexpected behavior, leave unsanitized.
+		const sanitizedContent = content;
+
+		const response = await updateSection(id, {
+			published,
+			order,
+			title,
+			slug,
+			subtitle,
+			image,
+			thumbnail,
+			altTitle,
+			content: sanitizedContent,
+			...(projects.length && { projects })
+		});
+
+		if (!response.success) {
+			return fail(400, response);
+		}
+
+		return response;
+	}
+} satisfies Actions;
---
+++ src/routes/(admin)/admin/(layout)/sections/edit/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import SectionForm from '$lib/components/SectionForm.svelte';
+	import type { ActionData, PageData } from './$types.js';
+
+	export let data: PageData;
+	export let form: ActionData;
+
+	$: console.log(form);
+
+	$: section = data.section;
+</script>
+
+<SectionForm {section} {form} />
+
+<style>
+</style>
