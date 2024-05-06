eb170292b7d240eb12629b224b3a8a696ad48246
paparomatoe <papatomatoe.project@gmail.com>
2024-05-06 16:30:13 +0300

    add SectionLink table render component

---
+++ src/lib/components/SectionLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	$: sectionsId = $$props?.sectionId ?? '';
+	$: sectionTile = $$props?.section?.title ?? '';
+</script>
+
+<a class="section-link" href={`/admin/sections/edit/${sectionsId}`}>{sectionTile}</a>
+
+<style>
+	.section-link {
+		color: var(--color--primary);
+	}
+</style>
