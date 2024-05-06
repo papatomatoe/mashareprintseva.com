592d2d69dd5682eeb56f7e7e8e509e20f6f07aa6
paparomatoe <papatomatoe.project@gmail.com>
2024-05-06 16:32:31 +0300

    add createProject function, add section info in each project in array

--- src/lib/services/projects.ts
+++ src/lib/services/projects.ts
@@ -1,10 +1,36 @@
+import { PRISMA_ERROR } from '$lib/constants/prismaErrors';
 import { db } from '$lib/database/db';
-
+import type { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { getSections } from '$lib/services/sections';
 export const getProjects = async () => {
 	try {
 		const projects = await db.project.findMany();
-		return projects;
+		const sections = await getSections();
+		const projectWithSection = projects.map((project) => ({
+			...project,
+			section: sections?.find((section) => section.id === project.sectionId)
+		}));
+		return projectWithSection;
 	} catch (e) {
 		console.error(e);
+	}
+};
+export const createProject = async (data: any) => {
+	try {
+		await db.project.create({
+			data
+		});
+
+		return { success: true };
+	} catch (e) {
+		console.log(e);
+		const prismaError = e as PrismaClientKnownRequestError;
+
+		const errorMessage = PRISMA_ERROR[prismaError.code];
+		const errorTarget = prismaError.meta?.target;
+
+		const message = errorTarget ? `${errorMessage}: "${errorTarget}"` : prismaError.message;
+
+		return { success: false, message };
 	}
 };
