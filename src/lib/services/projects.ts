ac755fa8250e5818d7b73fd6edae67188850a16f
paparomatoe <papatomatoe.project@gmail.com>
2024-05-07 19:53:31 +0300

    fix updating section in create/update project functions

--- src/lib/services/projects.ts
+++ src/lib/services/projects.ts
@@ -3,48 +3,53 @@ import type { PrismaClientKnownRequestError } from '@prisma/client/runtime/libra
 import { getPrismaError } from '$lib/services/error';
 export const getProjects = async () => {
 	try {
-		const projects = await db.project.findMany();
-		const sections = await db.section.findMany();
-		const projectWithSection = projects.map((project) => ({
-			...project,
-			section: sections?.find((section) => section.id === project.sectionId)
-		}));
-		return projectWithSection;
+		const projects = await db.project.findMany({ include: { section: true } });
+		return projects;
 	} catch (e) {
 		console.error(e);
 	}
 };
 export const getProject = async (id: string) => {
 	try {
-		const project = await db.project.findUnique({ where: { id } });
-		const section = await db.section.findUnique({ where: { id: project?.sectionId ?? '' } });
-
-		return { ...project, section };
+		const project = await db.project.findUnique({ where: { id }, include: { section: true } });
+		return project;
 	} catch (e) {
 		console.error(e);
 	}
 };
 export const createProject = async (data: any) => {
+	const { section, ...projectData } = data;
 	try {
 		await db.project.create({
-			data
+			data: {
+				...projectData,
+				...(section?.id && { section: { connect: { id: section.id } } })
+			}
 		});
 
 		return { success: true };
 	} catch (e) {
-		console.log(e);
+		console.error(e);
 		const message = getPrismaError(e as PrismaClientKnownRequestError);
 
 		return { success: false, message };
 	}
 };
 export const updateProject = async (id: string, data: any) => {
+	const { section, ...projectData } = data;
+
 	try {
-		await db.project.update({ where: { id }, data });
+		await db.project.update({
+			where: { id },
+			data: {
+				...projectData,
+				section: section ? { connect: { id: section.id } } : { disconnect: true }
+			}
+		});
 
 		return { success: true };
 	} catch (e) {
-		console.log(e);
+		console.error(e);
 
 		const message = getPrismaError(e as PrismaClientKnownRequestError);
 
