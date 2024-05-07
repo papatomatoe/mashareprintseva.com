fed366287947f8f04585fa7969e28f3682b9c633
paparomatoe <papatomatoe.project@gmail.com>
2024-05-07 19:47:02 +0300

    add delete projects api

---
+++ src/routes/api/v2/projects/delete/+server.ts
@@ -0,0 +1,44 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { db } from '$lib/database/db';
+
+export const DELETE: RequestHandler = async ({ request }) => {
+	const ids: string[] = await request.json();
+
+	if (!ids || ids.length === 0) throw error(400, 'Invalid Request Data');
+
+	try {
+		const { count } = await db.project.deleteMany({
+			where: {
+				id: {
+					in: ids
+				}
+			}
+		});
+
+		let deletedIds = [];
+
+		if (count === ids.length) {
+			deletedIds = [...ids];
+		} else {
+			const notDeletedProjects = await db.project.findMany({
+				where: {
+					id: {
+						in: ids
+					}
+				}
+			});
+
+			deletedIds = [
+				...notDeletedProjects
+					.filter((section) => ids.includes(section.id))
+					.map((section) => section.id)
+			];
+		}
+
+		return json(deletedIds);
+	} catch (e) {
+		console.log(e);
+		throw error(500, 'Something went wrong...');
+	}
+};
