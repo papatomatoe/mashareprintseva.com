98d95b0a4a8c380e85fbe32064a2cdefb4387d39
paparomatoe <papatomatoe.project@gmail.com>
2024-05-09 20:35:37 +0300

    added delete, update social items

--- src/routes/(admin)/admin/(layout)/socials/+page.server.ts
+++ src/routes/(admin)/admin/(layout)/socials/+page.server.ts
@@ -1,7 +1,6 @@
 import { fail, redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { getSocials, createSocial } from '$lib/services/socials';
-import { db } from '$lib/database/db';
+import { getSocials, createSocial, deleteSocial, updateSocial } from '$lib/services/socials';
 
 export const load = (async ({ locals }) => {
 	if (!locals.user) redirect(302, '/admin/login');
@@ -26,11 +25,50 @@ export const actions = {
 			return fail(400, { invalid: true });
 		}
 
-		try {
-			await createSocial({ title, link, icon });
-			return { success: true };
-		} catch (e) {
-			return fail(400, { success: false });
+		const response = await createSocial({ title, link, icon });
+		if (!response.success) {
+			return fail(400, response);
 		}
+
+		return response;
+	},
+	delete: async ({ request }) => {
+		const data = await request.formData();
+		const id = data.get('id');
+
+		if (typeof id !== 'string') {
+			return fail(400, { invalid: true });
+		}
+
+		const response = await deleteSocial(id);
+		if (!response.success) {
+			return fail(400, response);
+		}
+
+		return response;
+	},
+	edit: async ({ request }) => {
+		const data = await request.formData();
+
+		const id = data.get('id');
+		const title = data.get('title');
+		const link = data.get('link');
+		const icon = data.get('icon');
+
+		if (
+			typeof id !== 'string' ||
+			typeof title !== 'string' ||
+			typeof link !== 'string' ||
+			typeof icon !== 'string'
+		) {
+			return fail(400, { invalid: true });
+		}
+
+		const response = await updateSocial(id, { title, link, icon });
+		if (!response.success) {
+			return fail(400, response);
+		}
+
+		return response;
 	}
 } satisfies Actions;
--- src/routes/(admin)/admin/(layout)/socials/+page.svelte
+++ src/routes/(admin)/admin/(layout)/socials/+page.svelte
@@ -7,22 +7,44 @@
 	import Input from '$lib/components/Input.svelte';
 	import ConfirmPanel from '$lib/components/ConfirmPanel.svelte';
 	import type { PageData } from './$types';
+	import Edit from '$lib/icons/Edit.svelte';
+	import Delete from '$lib/icons/Delete.svelte';
+	import type { ActionResult } from '@sveltejs/kit';
+	import type { Social } from '@prisma/client';
 
 	export let data: PageData;
 
 	$: socials = data.socials;
 
-	let modal: SvelteComponent;
+	let createModal: SvelteComponent;
+	let deleteModal: SvelteComponent;
 
-	let icon;
+	const MODAL_TYPE = {
+		create: {
+			action: '?/create',
+			title: (item?: string) => 'Create Social Item'
+		},
+		edit: {
+			action: '?/edit',
+			title: (item?: string) => `Edit ${item}`
+		}
+	};
+
+	let modal: Record<string, any> | null = null;
+
+	let id = '';
+
+	let icon = '';
+	let iconError = '';
 
 	let title = '';
 	let titleError = '';
+	let oldTitle = '';
 
 	let link = '';
 	let linkError = '';
 
-	$: hasErrors = linkError || titleError;
+	$: hasErrors = linkError || titleError || iconError;
 
 	const handleClearTitleError = () => {
 		titleError = '';
@@ -32,55 +54,99 @@
 		linkError = '';
 	};
 
+	const handleClearIconError = () => {
+		iconError = '';
+	};
+
 	const handleSubmit = () => {
 		if (!title) titleError = 'required';
 		if (!link) linkError = 'required';
+		if (!icon) iconError = 'required';
 	};
 
 	const handleClearFields = () => {
+		icon = '';
+		iconError = '';
 		title = '';
 		titleError = '';
 		link = '';
 		linkError = '';
+		id = '';
+		oldTitle = '';
 	};
 
-	const handleCloseModal = () => {
+	const handleCloseCreateModal = () => {
 		handleClearFields();
-		modal.close();
+		createModal.close();
+		modal = null;
 	};
 
-	const handleAddNewItem = () => console.log('add new item');
-	const handleDeleteItems = (e: CustomEvent) => console.log('delete selected items');
-	const handleEditItem = (e: CustomEvent) => console.log('edit item ' + e.detail.id);
+	const handleCloseDeleteModal = () => {
+		handleClearFields();
+		deleteModal.close();
+	};
+
+	const handleOpenCreateModal = () => {
+		modal = MODAL_TYPE.create;
+		createModal.open();
+	};
+
+	const handleOpenDeleteModal = (social: Social) => {
+		title = social.title;
+		id = social.id;
+		deleteModal.open();
+	};
+
+	const handleOpenEditModal = (socials: Social) => {
+		title = socials.title;
+		oldTitle = socials.title;
+		id = socials.id;
+		icon = socials.icon;
+		link = socials.link;
+		modal = MODAL_TYPE.edit;
+		createModal.open();
+	};
+
+	const handleEnhance = ({ cancel }: { formElement: HTMLFormElement; cancel: () => void }) => {
+		if (hasErrors) cancel();
+		return async ({ result }: { result: ActionResult }) => {
+			if (result.type === 'failure') {
+				const error = result.data;
+
+				if (error?.target === 'title') titleError = error.message;
+				return;
+			}
+			if (result.type === 'success') {
+				createModal && handleCloseCreateModal();
+				deleteModal && handleCloseDeleteModal();
+			}
+			invalidateAll();
+			await applyAction(result);
+		};
+	};
 </script>
 
 <Modal
-	bind:this={modal}
+	bind:this={createModal}
 	on:close-modal={handleClearFields}
 	width="650px"
 	height="fit-content"
 	padding="30px"
 >
-	<form
-		method="POST"
-		action="?/create"
-		on:submit={handleSubmit}
-		use:enhance={({ cancel }) => {
-			if (hasErrors) cancel();
-			return async ({ result }) => {
-				if (result.type === 'redirect') {
-					goto(result.location);
-				} else {
-					invalidateAll();
-					await applyAction(result);
-				}
-			};
-		}}
-	>
-		<h2 class="social-modal__title">Add Social Item</h2>
+	<form method="POST" action={modal?.action} on:submit={handleSubmit} use:enhance={handleEnhance}>
+		<input type="hidden" name="id" bind:value={id} />
+		<h2 class="social-modal__title">{modal?.title(oldTitle)}</h2>
 		<div class="social-modal__container">
 			<div class="social-modal__file">
-				<File label="icon" name="icon" required />
+				<File
+					label="icon"
+					name="icon"
+					required
+					bind:fileUrl={icon}
+					bind:thumbnail={icon}
+					bind:error={iconError}
+					on:clearError={handleClearIconError}
+				/>
 			</div>
 			<div class="social-modal__input social-modal__input--title">
 				<Input
@@ -104,7 +170,23 @@
 			</div>
 		</div>
 		<div class="social-modal__panel">
-			<ConfirmPanel confirmText="Save" confirmType="submit" on:cancel={handleCloseModal} />
+			<ConfirmPanel confirmText="Save" confirmType="submit" on:cancel={handleCloseCreateModal} />
+		</div>
+	</form>
+</Modal>
+<Modal
+	bind:this={deleteModal}
+	on:close-modal={handleClearFields}
+	width="350px"
+	height="fit-content"
+	padding="30px"
+>
+	<form method="POST" action="?/delete" use:enhance={handleEnhance}>
+		<h2 class="social-modal__title">Delete item</h2>
+		<p>Do you want to delete <b>{title}</b> item?</p>
+		<input type="hidden" name="id" bind:value={id} />
+		<div class="social-modal__panel">
+			<ConfirmPanel confirmText="Delete" confirmType="submit" on:cancel={handleCloseDeleteModal} />
 		</div>
 	</form>
 </Modal>
@@ -117,6 +199,20 @@
 			{#if socials?.length}
 				{#each socials as social (social.id)}
 					<li class="social__item">
+						<div class="social__item-buttons">
+							<button
+								class="button social__item-button"
+								type="button"
+								aria-label="delete {social.title} item"
+								on:click={() => handleOpenDeleteModal(social)}><Delete /></button
+							>
+							<button
+								class="button social__item-button"
+								type="button"
+								aria-label="edit {social.title} item"
+								on:click={() => handleOpenEditModal(social)}><Edit /></button
+							>
+						</div>
 						<a
 							class="social__link"
 							href={social.link}
@@ -139,10 +235,8 @@
 				<button
 					class="button social__button"
 					type="button"
-					aria-label="add social menu item"
-					on:click={() => {
-						modal.open();
-					}}
+					aria-label="add new social menu item"
+					on:click={handleOpenCreateModal}
 				></button>
 			</li>
 		</ul>
@@ -162,6 +256,7 @@
 	}
 
 	.social__item {
+		position: relative;
 		display: flex;
 		flex-direction: column-reverse;
 		gap: 10px;
@@ -172,6 +267,48 @@
 		padding: 10px;
 		border: 1px solid var(--color--gray-85);
 		border-radius: 4px;
+	}
+
+	.social__item:hover .social__item-buttons {
+		opacity: 1;
+	}
+
+	.social__item-buttons {
+		opacity: 0;
+		top: 0;
+		right: 0;
+		padding: 5px;
+		position: absolute;
+		display: grid;
+		/* gap: 5px; */
+	}
+
+	.social__item-button {
+		width: 30px;
+		height: 30px;
+		padding: 0;
+		display: grid;
+		place-items: center;
+		background-color: var(--color--white);
+		--color--icon: var(--color--gray-15);
+	}
+
+	.social__item-button:hover,
+	.social__item-button:focus-visible {
+		background-color: var(--color--gray-85);
+	}
+	.social__item-button:active {
+		background-color: var(--color--gray-50);
+		--color--icon: var(--color--white);
+	}
+
+	.social__item-buttons:has(.social__item-button:focus) {
+		opacity: 1;
+	}
+
+	:global(.social__item-button svg) {
+		width: 17px;
+		height: 17px;
 	}
 
 	.social__icon {
