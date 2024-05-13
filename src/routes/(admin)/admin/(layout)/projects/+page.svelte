4dbc712187a766581a3e0efa493616b7a76f47f2
paparomatoe <papatomatoe.project@gmail.com>
2024-05-14 00:10:35 +0300

    added search, pagination, delete confirm

--- src/routes/(admin)/admin/(layout)/projects/+page.svelte
+++ src/routes/(admin)/admin/(layout)/projects/+page.svelte
@@ -8,27 +8,30 @@
 	import Status from '$lib/components/Status.svelte';
 	import type { ITableConfig, ITableData, IProject } from '$lib/components/Table.svelte';
 	import type { PageData } from './$types';
+	import Modal from '$lib/components/Modal.svelte';
+	import ConfirmPanel from '$lib/components/ConfirmPanel.svelte';
+	import type { SvelteComponent } from 'svelte';
 	export let data: PageData;
 
 	let loading = false;
 	let notification: { message: string; type: NotificationType } | null = null;
 
+	let deleteModal: SvelteComponent;
+	let selectedItemIds: string[] = [];
+
 	$: projects = data.projects;
+	$: pagination = data.pagination;
 
 	$: tableData = {
 		type: 'projects',
-		data: projects
+		data: projects,
+		pagination: pagination
 	} as unknown as ITableData; // NOTE: wtf???
 
 	let config: ITableConfig[] = [
 		{
 			key: 'select'
 		},
-		// {
-		// 	key: 'id',
-		// 	title: 'id',
-		// 	sortable: true
-		// },
 		{
 			key: 'image',
 			title: 'Image',
@@ -66,21 +69,75 @@
 			key: 'edit'
 		}
 	];
+	export const fetchProjectsData = async (
+		params = {
+			page: 0,
+			perPage: 50
+		}
+	) => {
+		loading = true;
+		try {
+			const response = await fetch('/api/v2/projects/list', {
+				method: 'POST',
+				body: JSON.stringify(params)
+			});
+			const data = await response.json();
 
+			projects = data.projects;
+			pagination = data.pagination;
+		} catch (e) {
+			console.error(e);
+			notification = { message: 'Something went wrong...', type: 'error' };
+		} finally {
+			loading = false;
+		}
+	};
+
+	const handleSelectPerPage = async (e: CustomEvent) => {
+		const { perPage } = e.detail;
+
+		await fetchProjectsData({ page: 0, perPage });
+	};
+
+	const handleSelectPage = async (e: CustomEvent) => {
+		const { page, perPage } = e.detail;
+		await fetchProjectsData({ page, perPage });
+	};
+	const handleSearch = async (e: CustomEvent) => {
+		const search = e.detail;
+
+		loading = true;
+
+		try {
+			const response = await fetch('/api/v2/projects/search', {
+				method: 'POST',
+				body: JSON.stringify(search)
+			});
+			const searchedResult = await response.json();
+			projects = searchedResult;
+		} catch (e) {
+			console.error(e);
+			notification = { message: 'Something went wrong...', type: 'error' };
+		} finally {
+			loading = false;
+		}
+	};
+	const handleClearSelectedItemIds = () => {
+		selectedItemIds = [];
+	};
 	const handleDeleteItems = async (e: CustomEvent) => {
-		const ids = e.detail;
-
 		loading = true;
 
 		try {
 			const response = await fetch('/api/v2/projects/delete', {
 				method: 'DELETE',
-				body: JSON.stringify(ids)
+				body: JSON.stringify(selectedItemIds)
 			});
 
 			const deletedIds = await response.json();
 
 			projects = projects?.filter((project) => !deletedIds.includes(project.id)) ?? [];
+			handleClearSelectedItemIds();
 
 			notification = { message: 'Successfully deleted', type: 'success' };
 		} catch (e) {
@@ -93,6 +150,15 @@
 	const handleEditItem = async (e: CustomEvent) => {
 		await goto(`/admin/projects/edit/${e.detail.id}`);
 	};
+	const handleOpenDeleteItemsDialog = (e: CustomEvent) => {
+		const ids = e.detail;
+		selectedItemIds = ids;
+		deleteModal.open();
+	};
+
+	const handleCloseDeleteModal = () => {
+		deleteModal.close();
+	};
 </script>
 
 <Notification
@@ -101,10 +167,41 @@
 	show={Boolean(notification)}
 />
 
+<Modal
+	bind:this={deleteModal}
+	on:close-modal={handleClearSelectedItemIds}
+	width="350px"
+	height="fit-content"
+	padding="30px"
+>
+	<h2 class="delete-modal__title">Delete projects</h2>
+	<p>Do you want to delete selected projects?</p>
+	<div class="delete-modal__panel">
+		<ConfirmPanel
+			confirmText="Delete"
+			on:confirm={handleDeleteItems}
+			on:cancel={handleCloseDeleteModal}
+		/>
+	</div>
+</Modal>
+
 <AdminTable
 	title="Projects"
 	{tableData}
 	{config}
-	on:delete={handleDeleteItems}
+	on:delete={handleOpenDeleteItemsDialog}
 	on:edit={handleEditItem}
+	on:search={handleSearch}
+	on:select-per-page={handleSelectPerPage}
+	on:select-page={handleSelectPage}
 />
+
+<style>
+	.delete-modal__title {
+		color: var(--color--gray-30);
+		margin-bottom: 30px;
+	}
+	.delete-modal__panel {
+		padding-top: 40px;
+	}
+</style>
