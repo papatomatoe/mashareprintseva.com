30bfc6f1702f17d5fb211e1393f9aaf0e4e0520d
paparomatoe <papatomatoe.project@gmail.com>
2024-05-14 00:13:19 +0300

    added pagination

--- src/routes/(admin)/admin/(layout)/projects/+page.server.ts
+++ src/routes/(admin)/admin/(layout)/projects/+page.server.ts
@@ -5,10 +5,11 @@ import { getProjects } from '$lib/services/projects';
 export const load = (async ({ locals }) => {
 	if (!locals.user) redirect(302, '/admin/login');
 
-	const projects = await getProjects();
+	const data = await getProjects();
 
 	return {
 		pageTitle: 'Admin | Projects',
-		projects
+		projects: data?.projects,
+		pagination: data?.pagination
 	};
 }) satisfies PageServerLoad;
