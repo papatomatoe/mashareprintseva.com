33b62f6862e6a0aabdb156e8a98dbeb0644bcd74
papatomatoe <papatomatoe.project@gmail.com>
2024-05-16 23:34:20 +0300

    refactoring

--- src/routes/(admin)/admin/(layout)/main-page/+page.server.ts
+++ src/routes/(admin)/admin/(layout)/main-page/+page.server.ts
@@ -1,12 +1,12 @@
 import { redirect, fail } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 import { db } from '$lib/database/db';
+import { getMainPageData } from '$lib/services/main';
 
 export const load = (async ({ locals }) => {
 	if (!locals.user) redirect(302, '/admin/login');
 
-	const mainPage = await db.mainPage.findFirst();
-
+	const mainPage = await getMainPageData();
 	return {
 		mainPage,
 		pageTitle: 'Admin | Main Page'
