4b17c9dd07b900c6a5431e10a32f078121a176be
papatomatoe <papatomatoe.project@gmail.com>
2024-11-12 22:08:59 +0300

    add: socials api

---
+++ src/routes/api/socials/+server.ts
@@ -0,0 +1,25 @@
+import { json } from '@sveltejs/kit';
+import type { Social } from '$lib/types/socials';
+
+async function getSocials() {
+	const socials: Social[] = [];
+
+	const paths = import.meta.glob('/src/content/socials/*.md', { eager: true });
+
+	for (const path in paths) {
+		const file = paths[path];
+		const slug = path.split('/').at(-1)?.replace('.md', '');
+
+		if (file && typeof file === 'object' && 'metadata' in file && slug) {
+			const social = file.metadata as Social;
+			socials.push(social);
+		}
+	}
+
+	return socials;
+}
+
+export async function GET() {
+	const socials = await getSocials();
+	return json(socials);
+}
