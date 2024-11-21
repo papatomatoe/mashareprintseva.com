75dff0c59359a8da565662e2771375fa6818b60c
papatomatoe <papatomatoe.project@gmail.com>
2024-11-21 22:52:27 +0300

    update custom img component

--- src/lib/components/custom/img.svelte
+++ src/lib/components/custom/img.svelte
@@ -5,6 +5,28 @@
 	};
 
 	let { src, alt }: Props = $props();
+
+	const image = $derived.by(() => {
+		const [path, extension] = src.split('.') ?? [];
+		return { path: path ?? '', extension: extension ?? '' };
+	});
 </script>
 
-<img {src} {alt} loading="lazy" width="650" height="650" />
+<picture>
+	<source
+		media="(min-width: 1200px)"
+		srcset="{image.path}-desk.{image.extension}, {image.path}-desk@2x.{image.extension} 2x"
+	/>
+	<source
+		media="(min-width: 768px)"
+		srcset="{image.path}-tab.{image.extension}, {image.path}-tab@2x.{image.extension} 2x"
+	/>
+	<img
+		src="{image.path}-mob.{image.extension}"
+		srcset="{image.path}-mob@2x.{image.extension} 2x"
+		width="326"
+		height="326"
+		loading="lazy"
+		{alt}
+	/>
+</picture>
