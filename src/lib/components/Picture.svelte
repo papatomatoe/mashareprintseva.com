e7f33140c06e73d5c93aa4a0fc95adbd8e98a591
papatomatoe <papatomatoe.project@gmail.com>
2024-11-22 21:25:13 +0300

    add: placeholder and show effect

--- src/lib/components/Picture.svelte
+++ src/lib/components/Picture.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import type { Action } from 'svelte/action';
+
 	type Props = {
 		class?: string;
 		src: string;
@@ -8,15 +10,41 @@
 	};
 
 	let { class: className, src, alt, height, width }: Props = $props();
+
+	let loading = $state(true);
+
+	const handleOnLoad = () => {
+		loading = false;
+	};
 </script>
 
-<picture class={className}>
+<picture
+	class={className}
+	class:placeholder={loading}
+	style="--width: {width}px; --height: {height}px"
+>
 	<source media="(min-width: 1200px)" srcset="{src}-desk.avif, {src}-desk@2x.avif 2x" />
 	<source media="(min-width: 768px)" srcset="{src}-tab.avif, {src}-tab@2x.avif 2x" />
-	<img src="{src}-mob.avif" srcset="{src}-mob@2x.avif 2x" {width} {height} loading="lazy" {alt} />
+	<img
+		class:blurry={loading}
+		src="{src}-mob.avif"
+		srcset="{src}-mob@2x.avif 2x"
+		{width}
+		{height}
+		loading="lazy"
+		{alt}
+		onload={handleOnLoad}
+	/>
 </picture>
 
 <style>
+	.placeholder {
+		background-color: #f6f6f6;
+	}
+
+	.blurry {
+		filter: contrast(0.1) brightness(2) grayscale(2) blur(30px);
+	}
 	picture {
 		display: block;
 		width: 100%;
@@ -25,5 +53,6 @@
 	img {
 		width: 100%;
 		height: auto;
+		transition: all 0.3s;
 	}
 </style>
--- src/lib/components/custom/img.svelte
+++ src/lib/components/custom/img.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Picture from '$lib/components/Picture.svelte';
 	type Props = {
 		src: string;
 		alt: string;
@@ -7,26 +8,9 @@
 	let { src, alt }: Props = $props();
 
 	const image = $derived.by(() => {
-		const [path, extension] = src.split('.') ?? [];
-		return { path: path ?? '', extension: extension ?? '' };
+		const path = src.split('.');
+		return path && path[0];
 	});
 </script>
 
-<picture>
-	<source
-		media="(min-width: 1200px)"
-		srcset="{image.path}-desk.{image.extension}, {image.path}-desk@2x.{image.extension} 2x"
-	/>
-	<source
-		media="(min-width: 768px)"
-		srcset="{image.path}-tab.{image.extension}, {image.path}-tab@2x.{image.extension} 2x"
-	/>
-	<img
-		src="{image.path}-mob.{image.extension}"
-		srcset="{image.path}-mob@2x.{image.extension} 2x"
-		width="326"
-		height="326"
-		loading="lazy"
-		{alt}
-	/>
-</picture>
+<Picture src={image} {alt} width="326" height="320" />
