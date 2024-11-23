655ac96280fce8c4fb1bd47f413a13a329818792
papatomatoe <papatomatoe.project@gmail.com>
2024-11-23 19:34:20 +0300

    update: convert images script

--- utils/convert.js
+++ utils/convert.js
@@ -1,31 +1,32 @@
-import { readdir, readFile, unlink } from 'node:fs/promises';
+import { readdir, readFile } from 'node:fs/promises';
 import sharp from 'sharp';
 
-const DIR_PATH = './static/images';
+const IMAGES_PATH = { source: './images', dist: './static/images' };
 const fileTypes = ['jpeg', 'jpg', 'png'];
 
 async function convertToAvif() {
 	try {
-		const fileNames = await readdir(DIR_PATH, { withFileTypes: true });
+		const fileNames = await readdir(IMAGES_PATH.source, { withFileTypes: true });
 
-		fileNames.forEach(async (file) => {
+		const promises = fileNames.map(async (file) => {
 			const isFile = file.isFile();
 			const splittedFileType = file.name.split('.');
 			const fileType = splittedFileType[splittedFileType.length - 1];
 			const fileName = splittedFileType.slice(0, splittedFileType.length - 1).join('.');
-			const filePath = `${DIR_PATH}/${file.name}`;
+			const filePath = `${IMAGES_PATH.source}/${file.name}`;
 
 			if (isFile && fileTypes.includes(fileType)) {
 				const buffer = await readFile(filePath);
-
-				await sharp(buffer)
-					.webp({ quality: 85 })
-					.toFile(`${DIR_PATH}/${fileName}.avif`)
-					.then(() => {
-						unlink(filePath);
-					});
+				const path = `${IMAGES_PATH.dist}/${fileName}.avif`;
+				const job = sharp(buffer);
+				const avif = job.avif({ quality: 70 });
+				const file = await avif.toFile(path);
+				console.log('SUCCESS: ', path);
+				return file;
 			}
 		});
+
+		await Promise.all(promises);
 		console.log('✓ DONE');
 	} catch (err) {
 		console.error(err);
