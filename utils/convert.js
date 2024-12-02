b3260645aef2a06675cce654b0763d7c41cb5530
papatomatoe <papatomatoe.project@gmail.com>
2024-12-02 23:17:34 +0300

    update: file processing script

--- utils/convert.js
+++ utils/convert.js
@@ -1,68 +1,129 @@
-import { readdir, readFile, unlink } from 'node:fs/promises';
+import { readdir, readFile, unlink, copyFile } from 'node:fs/promises';
 import sharp from 'sharp';
 
-const IMAGES_PATH = { source: './images', dist: './static/images' };
-const fileTypes = ['jpeg', 'jpg', 'png'];
+const FILE_PATH = { source: './uploads', images: './static/images', files: './static/files' };
+const IMAGE_TYPES = ['jpeg', 'jpg', 'png'];
+const DOC_TYPES = ['pdf'];
 
-async function convertToAvif() {
-	try {
-		const sourceImages = await readdir(IMAGES_PATH.source, { withFileTypes: true });
-		const distImages = await readdir(IMAGES_PATH.dist);
+const checkFileType = (fileType) =>
+	(IMAGE_TYPES.includes(fileType) && 'image') || (DOC_TYPES.includes(fileType) && 'pdf');
 
-		const _sourceImages = sourceImages.map((file) => file.name.split('.')[0]);
-		const _distImages = distImages.map((path) => path.split('.')[0]);
-		const setSource = new Set(_sourceImages);
-		const distSource = new Set(_distImages);
+async function processDocuments(sourceFiles) {
+	const distDocs = await readdir(FILE_PATH.files);
 
-		if (sourceImages.length > distImages.length) {
-			const addedFilesSet = setSource.difference(distSource);
+	const sourceDocs = sourceFiles.filter((file) => DOC_TYPES.includes(file.name.split('.').at(-1)));
 
-			const addedFiles = [...addedFilesSet];
+	const _sourceDocs = sourceDocs.map((file) => file.name.split('.').at(0));
+	const _distDocs = distDocs.map((path) => path.split('.')[0]);
 
-			const newFiles = sourceImages.filter((file) => addedFiles.includes(file.name.split('.')[0]));
+	const setSourceDocs = new Set(_sourceDocs);
+	const setDistDocs = new Set(_distDocs);
 
-			const promises = newFiles.map(async (file) => {
-				const isFile = file.isFile();
-				const splittedFileType = file.name.split('.');
-				const fileType = splittedFileType[splittedFileType.length - 1];
-				const fileName = splittedFileType.slice(0, splittedFileType.length - 1).join('.');
-				const filePath = `${IMAGES_PATH.source}/${file.name}`;
+	if (setSourceDocs.size > setDistDocs.size) {
+		const addedDocsSet = setSourceDocs.difference(setDistDocs);
+		const addedDocs = [...addedDocsSet];
+		const newDocs = sourceDocs.filter((doc) => addedDocs.includes(doc.name.split('.').at(0)));
 
-				if (isFile && fileTypes.includes(fileType)) {
-					const buffer = await readFile(filePath);
-					const path = `${IMAGES_PATH.dist}/${fileName}.avif`;
-					const job = sharp(buffer);
-					const avif = job.avif({ quality: 70 });
-					const file = await avif.toFile(path);
-					console.log('SUCCESS: ', path);
-					return file;
-				}
+		const promises = newDocs.map(async (doc) => {
+			const docDistPath = `${FILE_PATH.files}/${doc.name}`;
+			const docSourcePath = `${FILE_PATH.source}/${doc.name}`;
+			await copyFile(docSourcePath, docDistPath);
+			console.log('COPY: ', docSourcePath, ' -> ', docDistPath);
+		});
+
+		await Promise.all(promises);
+		console.log('✓ DONE');
+	} else if (setSourceDocs.size < setDistDocs.size) {
+		const unlinkedDocsSet = setDistDocs.difference(setSourceDocs);
+		const unlinkedDocs = [...unlinkedDocsSet];
+
+		const deletedDocs = distDocs.filter((doc) => unlinkedDocs.includes(doc.split('.').at(0)));
+
+		if (deletedDocs.length) {
+			const promises = deletedDocs.map(async (file) => {
+				const path = `${FILE_PATH.files}/${file}`;
+				const unlinked = await unlink(path);
+				console.log('DELETED: ', path);
+				return unlinked;
 			});
-
 			await Promise.all(promises);
 			console.log('✓ DONE');
-		} else if (sourceImages.length < distImages.length) {
-			const unlinkedFilesSet = distSource.difference(setSource);
-
-			const unlinkedFiles = [...unlinkedFilesSet];
-
-			if (unlinkedFiles.length) {
-				const promises = unlinkedFiles.map((file) => {
-					const path = `./static/images/${file}.avif`;
-					const u = unlink(path);
-					console.log('DELETED: ', path);
-					return u;
-				});
-
-				await Promise.all(promises);
-				console.log('✓ DONE');
-			}
-		} else {
-			console.log('CONVERT PASS: No new images');
 		}
+	} else {
+		console.log('DOCUMENTS COPY PASS: No new files\n');
+	}
+}
+
+async function processImages(sourceFiles) {
+	const distImages = await readdir(FILE_PATH.images);
+	const sourceImages = sourceFiles.filter((file) => {
+		return IMAGE_TYPES.includes(file.name.split('.').at(-1));
+	});
+
+	const _sourceImages = sourceImages.map((file) => file.name.split('.').at(0));
+	const _distImages = distImages.map((path) => path.split('.')[0]);
+
+	const setSourceImages = new Set(_sourceImages);
+	const setDistImages = new Set(_distImages);
+
+	if (setSourceImages.size > setDistImages.size) {
+		const addedImagesSet = setSourceImages.difference(setDistImages);
+
+		const addedImages = [...addedImagesSet];
+
+		const newFiles = sourceImages.filter((file) => addedImages.includes(file.name.split('.')[0]));
+
+		const promises = newFiles.map(async (file) => {
+			const isFile = file.isFile();
+			const splittedFileType = file.name.split('.');
+			const fileType = splittedFileType[splittedFileType.length - 1];
+			const fileName = splittedFileType.slice(0, splittedFileType.length - 1).join('.');
+			const filePath = `${FILE_PATH.source}/${file.name}`;
+
+			if (isFile) {
+				const checkedFileType = checkFileType(fileType);
+
+				if (!checkFileType || checkedFileType !== 'image') return;
+
+				const buffer = await readFile(filePath);
+				const path = `${FILE_PATH.images}/${fileName}.avif`;
+				const job = sharp(buffer);
+				const avif = job.avif({ quality: 70 });
+				const file = await avif.toFile(path);
+				console.log('SUCCESS: ', path);
+				return file;
+			}
+		});
+
+		await Promise.all(promises);
+		console.log('✓ DONE');
+	} else if (setSourceImages.size < setDistImages.size) {
+		const unlinkedFilesSet = setDistImages.difference(setSourceImages);
+		const unlinkedFiles = [...unlinkedFilesSet];
+		if (unlinkedFiles.length) {
+			const promises = unlinkedFiles.map((file) => {
+				const path = `./${FILE_PATH.images}/${file}.avif`;
+				const u = unlink(path);
+				console.log('DELETED: ', path);
+				return u;
+			});
+			await Promise.all(promises);
+			console.log('✓ DONE');
+		}
+	} else {
+		console.log('IMAGES CONVERT PASS: No new images');
+	}
+}
+
+async function filesProcess() {
+	try {
+		const sourceFiles = await readdir(FILE_PATH.source, { withFileTypes: true });
+
+		await processDocuments(sourceFiles);
+		await processImages(sourceFiles);
 	} catch (err) {
 		console.error(err);
 	}
 }
 
-convertToAvif();
+filesProcess();
