200f8ca52577f8965746a64a3701241242e4d7f2
papatomatoe <papatomatoe.project@gmail.com>
2025-08-09 01:23:13 +0300

    add: popover component

---
+++ src/lib/components/admin/Popover.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		trigger: Snippet;
+		children?: Snippet;
+		class?: string;
+		align?: 'start' | 'center' | 'end';
+		side?: 'top' | 'right' | 'bottom' | 'left';
+	};
+
+	let { trigger, children, class: className, side, align }: Props = $props();
+</script>
+
+<Popover.Root>
+	<Popover.Trigger>{@render trigger()}</Popover.Trigger>
+	<Popover.Content class={className} {side} {align}>{@render children()}</Popover.Content>
+</Popover.Root>
